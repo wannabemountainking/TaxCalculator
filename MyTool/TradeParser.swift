08d608fe12eb1d91d0f0000e7fd4b4dc1eb0a086
wannabemountainking <kdy0096@gmail.com>
2026-09-23 19:18:58 +0900

    parsing final

--- MyTool/TradeParser.swift
+++ MyTool/TradeParser.swift
@@ -8,15 +8,14 @@
 import Foundation
 
 
-enum ParsingError: Error {
-    case lackOfFieldsError(desc: String)
-    case transactionTypeError(desc: String)
-    case quantityTypeError(desc: String)
-    case unitPriceTypeError(desc: String)
-    case tradeFeeTypeError(desc: String)
-    case quantityTypeConversionError(desc: String)
-    case unitPriceTypeConversionError(desc: String)
-    case tradeFeeTypeConversionError(desc: String)
+enum ParsingError: String, Error {
+    case lackOfFieldsError
+    case transactionTypeError
+    case quantityError
+	case unitPriceTypeError
+    case tradeFeeTypeError
+    case unitPriceTypeConversionError
+    case tradeFeeTypeConversionError
 }
 
 struct TradeParser {
@@ -25,30 +24,27 @@ struct TradeParser {
 		let fields = line.split(separator: ",", omittingEmptySubsequences: false )
 			.map { String($0) }
 		guard fields.count == 6 else {
-            return Result.failure(.lackOfFieldsError(desc: "필드 개수가 맞지 않습니다"))
+            return Result.failure(.lackOfFieldsError)
 		}
 		let date = fields[0]
 		let ticker = fields[2]
 		guard let type = TransactionType(rawValue: fields[1]) else {
-            return .failure(.transactionTypeError(desc: "Transaction 타입이 아닙니다"))
+            return .failure(.transactionTypeError)
 		}
-		guard let _ = Int(fields[3]) else {
-            return .failure(.quantityTypeError(desc: "수량으로 적절하지 않은 타입입니다"))
+		guard let qtyInt = Int(fields[3]), qtyInt >= 0, let qty = Decimal(string: fields[3]) else {
+            return .failure(.quantityError)
 		}
-		guard let _ = Double(fields[4]) else {
-            return .failure(.unitPriceTypeError(desc: "단가로 적절하지 않은 타입입니다"))
+		guard let unitPriceDouble = Double(fields[4]), unitPriceDouble >= 0 else {
+            return .failure(.unitPriceTypeError)
 		}
-		guard let _ = Double(fields[5]) else {
-            return .failure(.tradeFeeTypeError(desc: "중개수수료로 적절하지 않은 타입입니다"))
+		guard let tradeFeeDouble = Double(fields[5]), tradeFeeDouble >= 0 else {
+            return .failure(.tradeFeeTypeError)
 		}
-		guard let qty = Decimal(string: fields[3]), qty >= 0 else {
-            return .failure(.quantityTypeConversionError(desc: "수량의 타입변환(Int -> Decimal)에 실패했습니다"))
+		guard let price = Decimal(string: fields[4]) else {
+            return .failure(.unitPriceTypeConversionError)
 		}
-		guard let price = Decimal(string: fields[4]), price >= 0 else {
-            return .failure(.unitPriceTypeConversionError(desc: "단가의 타입변환(Double -> Decimal)에 실패했습니다"))
-		}
-		guard let fee = Decimal(string: fields[5]), fee >= 0 else {
-            return .failure(.tradeFeeTypeConversionError(desc: "중개수수료의 타입변환(Double -> Decimal)에 실패했습니다"))
+		guard let fee = Decimal(string: fields[5]) else {
+            return .failure(.tradeFeeTypeConversionError)
 		}
 		
         return .success(
--- MyTool/main.swift
+++ MyTool/main.swift
@@ -1,26 +1,37 @@
 import Foundation
 
 
-if let tradeURL = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask).first?.appending(component: "trades.csv", directoryHint: .notDirectory) {
+if let tradeURL = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask).first?.appending(components: "TaxCalculator", "trades.csv", directoryHint: .notDirectory) {
     
     
     let tradeText = try String(contentsOf: tradeURL, encoding: .utf8)
     let tradeTexts = tradeText.components(separatedBy: "\n").filter { !$0.isEmpty }
-    
-    var transactions: [Transaction] = []
-    
-    for (index, tradeData) in tradeTexts.enumerated() {
-        let result = TradeParser.parse(line: tradeData)
-        do {
-            let transaction = try result.get()
-            transactions.append(transaction)
-        } catch {
-            // TODO: 할 일을 할것
-        }
-//            transactions.append(transaction)
-//        } else {
-//            print("\(index) 번 \(tradeData.components(separatedBy: ",")[2]) \(TradeParser.parserError)")
-//        }
-    }
-    print(transactions)
+	
+	let transactions = tradeTexts.enumerated().compactMap { (index: Int, tradeContents: String) in
+		let result = TradeParser.parse(line: tradeContents)
+		switch result {
+		case .success(let transaction):
+			print(transaction)
+			return transaction
+		case .failure(let error):
+			switch error {
+			case .lackOfFieldsError:
+				print("\(index + 1)번째 줄, 에러 내용: 필드 개수가 맞지 않습니다")
+			case .transactionTypeError:
+				print("\(index + 1)번째 줄, 에러 내용: 적절한 Transaction 타입이 아닙니다")
+			case .quantityError:
+				print("\(index + 1)번째 줄, 에러 내용: 적절한 Quantity 타입이 아닙니다")
+			case .unitPriceTypeError:
+				print("\(index + 1)번째 줄, 에러 내용: 적절한 UnitPrice 타입이 아닙니다")
+			case .tradeFeeTypeError:
+				print("\(index + 1)번째 줄, 에러 내용: 적절한 중개수수료 타입이 아닙니다")
+			case .unitPriceTypeConversionError:
+				print("\(index + 1)번째 줄, 에러 내용: 단가의 Decimal 타입 변환에 실패했습니다")
+			case .tradeFeeTypeConversionError:
+				print("\(index + 1)번째 줄, 에러 내용: 중개수수료의 Decimal 타입 변환에 실패했습니다")
+			}
+		return nil
+		}
+	}
 }
+
